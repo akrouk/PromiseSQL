4197ef9b580c0017dda61ada134f8c05617aff75
Adam J. Krouk <akrouk2000@gmail.com>
2022-05-07 15:34:42 -0400

    db test

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 'use strict';
-const fs = require('fs');
-var file;
+var db;
+
+function open(file) {
+    db = new sqlite.Database(file, err => {
+        if (err) {
+            console.error(err, err.stack);
+        }
+    });
+
+    db.query = function (sql, params = []) {
+        return new Promise((res, rej) => {
+            this.all(sql, params, function (err, result) {
+                if (err) {
+                    rej(err);
+                } else {
+                    res(result);
+                }
+            });
+        });
+    }
+}
+
+function close() {
+    db.close();
+    db = undefined;
+}
 
 module.exports.file = function (filepath) {
-    file = fs.readFileSync(filepath);
-    console.log(file);
+    open(filepath);
+    const sql = `
+        CREATE TABLE IF NOT EXISTS table (
+            name TEXT NOT NULL UNIQUE,
+            id INT
+        );
+    `;
+    db.query(sql);
+    close();
 }
